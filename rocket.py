1cd0ec3c3737ef8d7b4d7c6c9c47de26c87d89d5
Adam Kelly <adam@cthulahoops.org>
2021-05-13 12:45:20 +0100

    Add basic garbage collection.

--- rocket.py
+++ rocket.py
@@ -37,6 +37,34 @@ class ClankyBotLauchSystem:
 
 LAUNCH_SYSTEM = ClankyBotLauchSystem()
 
+class GarbageCollectionBot:
+    def __init__(self):
+        self.garbage_bot = rctogether.create_bot(name="Garbage Collector", emoji="🛺", x=22, y=61)
+        self.garbage_queue = []
+        self.garbage = None
+
+    @property
+    def id(self):
+        return self.garbage_bot['id']
+
+    def add_garbage(self, garbage):
+        self.garbage_queue.append(garbage)
+        if self.garbage is None and len(self.garbage_queue) > 3:
+            self.collect()
+
+    def collect(self):
+        self.garbage = self.garbage_queue.pop(0)
+        print("Crew dispatched to collect: ", self.garbage)
+        rctogether.update_bot(self.id, self.garbage['pos'])
+
+    def handle_update(self, entity):
+        if self.garbage and entity['pos'] == self.garbage['pos']:
+            rctogether.delete_bot(self.garbage['id'])
+            self.garbage = None
+            rctogether.update_bot(self.id, {'x': 22, 'y': 61})
+
+GC_BOT = GarbageCollectionBot()
+
 def handle_entity(entity):
     person_name = entity.get("person_name")
     if person_name:
@@ -47,6 +75,7 @@ def handle_entity(entity):
         print("Target detected at: ", LAUNCH_SYSTEM.rocket, target_position)
         rctogether.update_bot(LAUNCH_SYSTEM.rocket["id"], target_position)
     elif entity.get("pos") == {'x': 27, 'y': 61}:
+        print("New instructions received: ", entity)
         note_text = entity.get("note_text")
         if note_text == '':
             LAUNCH_SYSTEM.target = "Nobody"
@@ -56,13 +85,17 @@ def handle_entity(entity):
             if LAUNCH_SYSTEM.target in TARGETS:
                 rctogether.update_bot(LAUNCH_SYSTEM.rocket["id"], TARGETS[LAUNCH_SYSTEM.target])
     elif entity['id'] == LAUNCH_SYSTEM.rocket["id"]:
+        LAUNCH_SYSTEM.rocket = entity
         rocket_position = entity['pos']
         target_position = TARGETS.get(LAUNCH_SYSTEM.target)
 
         print(rocket_position, target_position)
         if rocket_position == target_position:
             rctogether.update_bot(LAUNCH_SYSTEM.rocket["id"], {'emoji': random.choice(PAYLOADS)})
+            GC_BOT.add_garbage(LAUNCH_SYSTEM.rocket)
             LAUNCH_SYSTEM.respawn_rocket()
+    elif entity['id'] == GC_BOT.id:
+        GC_BOT.handle_update(entity)
 
 def handle_message(message):
     try:
