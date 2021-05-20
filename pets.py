b0a935a66d731616b5e614f3b175e0fc362eaa27
Adam Kelly <adam@cthulahoops.org>
2021-05-20 09:46:20 +0100

    Fix some formatting.

--- pets.py
+++ pets.py
@@ -1,50 +1,51 @@
-import eventlet
-eventlet.monkey_patch()
-
 import random
 import re
 import datetime
 from collections import defaultdict
-import rctogether
+
+import eventlet
+
+eventlet.monkey_patch()
+
+import rctogether # pylint: disable=wrong-import-position
 
 ANIMALS = [
-        {'emoji': '🐕', 'name': 'dog', 'noise': 'woof!'},
-        {'emoji': '🐈', 'name': 'cat', 'noise': 'miaow!'},
-        {'emoji': '🐁', 'name': 'mouse', 'noise': 'squeak!'},
-        {'emoji': '🦛', 'name': 'hippo'},
-        {'emoji': '🐸', 'name': 'frog', 'noise': 'ribbet!'},
-        {'emoji': '🦖', 'name': 't-rex', 'noise': 'RAWR!'},
-        {'emoji': '🦜', 'name': 'parrot', 'noise': 'HELLO!'},
-        {'emoji': '🐊', 'name': 'crocodile'},
-        {'emoji': '🦒', 'name': 'giraffe'},
-        {'emoji': '🦆', 'name': 'duck', 'noise': 'quack!'},
-        {'emoji': '🐑', 'name': 'sheep', 'noise': 'baa!'},
-        {'emoji': '🐢', 'name': 'turtle'},
-        {'emoji': '🐘', 'name': 'elephant'},
-        {'emoji': '🦉', 'name': 'owl', 'noise': 'hoot hoot!'},
-        {'emoji': '🐉', 'name': 'dragon', 'noise': '🔥'},
-        {'emoji': '🚀', 'name': 'rocket'},
-        {'emoji': '🦊', 'name': 'fox', 'noise': 'Wrahh!'}
-        ]
+    {"emoji": "🐕", "name": "dog", "noise": "woof!"},
+    {"emoji": "🐈", "name": "cat", "noise": "miaow!"},
+    {"emoji": "🐁", "name": "mouse", "noise": "squeak!"},
+    {"emoji": "🦛", "name": "hippo"},
+    {"emoji": "🐸", "name": "frog", "noise": "ribbet!"},
+    {"emoji": "🦖", "name": "t-rex", "noise": "RAWR!"},
+    {"emoji": "🦜", "name": "parrot", "noise": "HELLO!"},
+    {"emoji": "🐊", "name": "crocodile"},
+    {"emoji": "🦒", "name": "giraffe"},
+    {"emoji": "🦆", "name": "duck", "noise": "quack!"},
+    {"emoji": "🐑", "name": "sheep", "noise": "baa!"},
+    {"emoji": "🐢", "name": "turtle"},
+    {"emoji": "🐘", "name": "elephant"},
+    {"emoji": "🦉", "name": "owl", "noise": "hoot hoot!"},
+    {"emoji": "🐉", "name": "dragon", "noise": "🔥"},
+    {"emoji": "🚀", "name": "rocket"},
+    {"emoji": "🦊", "name": "fox", "noise": "Wrahh!"},
+]
 
-NOISES = {animal['emoji']: animal.get('noise', '💖') for animal in ANIMALS}
+NOISES = {animal["emoji"]: animal.get("noise", "💖") for animal in ANIMALS}
 
-GENIE_HOME = {'x': 60, 'y': 15}
+GENIE_HOME = {"x": 60, "y": 15}
 SPAWN_POINTS = [
-        {'x': 58, 'y': 15},
-        {'x': 58, 'y': 13},
-        {'x': 60, 'y': 13},
-        {'x': 62, 'y': 13},
-        {'x': 62, 'y': 15},
-        {'x': 62, 'y': 17},
-        {'x': 60, 'y': 17},
-    ]
+    {"x": 58, "y": 15},
+    {"x": 58, "y": 13},
+    {"x": 60, "y": 13},
+    {"x": 62, "y": 13},
+    {"x": 62, "y": 15},
+    {"x": 62, "y": 17},
+    {"x": 60, "y": 17},
+]
 
-
-    # print(entity)
 
 def position_tuple(pos):
-    return (pos['x'], pos['y'])
+    return (pos["x"], pos["y"])
+
 
 class Agency:
     def __init__(self):
@@ -54,26 +55,28 @@ class Agency:
         self.processed_message_dt = datetime.datetime.utcnow()
 
         for bot in rctogether.get_bots():
-            if bot['emoji'] == "🧞":
+            if bot["emoji"] == "🧞":
                 print("Found the genie!")
                 self.genie = rctogether.Bot(bot, print)
             else:
-                if bot.get('message'):
-                    [owner_id] = bot['message']['mentioned_entity_ids']
+                if bot.get("message"):
+                    [owner_id] = bot["message"]["mentioned_entity_ids"]
                     self.owned_animals[owner_id].append(rctogether.Bot(bot, print))
                 else:
-                    self.available_animals[position_tuple(bot['pos'])] = rctogether.Bot(bot, print)
+                    self.available_animals[position_tuple(bot["pos"])] = rctogether.Bot(
+                        bot, print
+                    )
 
     def restock_inventory(self):
         if not self.genie:
-            raise ValueError("No genie!")
             self.genie = subscription.create_bot(
                 name="Pet Agency Genie",
                 emoji="🧞",
-                x=GENIE_HOME['x'],
-                y=GENIE_HOME['y'],
+                x=GENIE_HOME["x"],
+                y=GENIE_HOME["y"],
                 can_be_mentioned=True,
-                handle_update=print)
+                handle_update=print,
+            )
 
         for pos in SPAWN_POINTS:
             if position_tuple(pos) not in self.available_animals:
@@ -81,15 +84,16 @@ class Agency:
 
     def spawn_animal(self, pos):
         animal = random.choice(ANIMALS)
-        while self.available(animal['emoji']):
+        while self.available(animal["emoji"]):
             animal = random.choice(ANIMALS)
 
         return subscription.create_bot(
-            name=animal['name'],
-            emoji=animal['emoji'],
-            x=pos['x'],
-            y=pos['y'],
-            handle_update=print)
+            name=animal["name"],
+            emoji=animal["emoji"],
+            x=pos["x"],
+            y=pos["y"],
+            handle_update=print,
+        )
 
     def available(self, animal):
         return any(x.emoji == animal for x in self.available_animals.values())
@@ -105,73 +109,103 @@ class Agency:
 
     def handle_mention(self, adopter, message):
         print(message)
-        m = re.search(r'adopt (a|an|the|one)? ([A-Za-z-]+),? please', message['text'], re.IGNORECASE)
+        m = re.search(
+            r"adopt (a|an|the|one)? ([A-Za-z-]+),? please",
+            message["text"],
+            re.IGNORECASE,
+        )
         if m:
             animal_name = m.groups()[1]
-            if animal_name == 'horse':
-                rctogether.send_message(self.genie.id, f"@**{adopter['person_name']}** Sorry, that's just a picture of a horse.")
+            if animal_name == "horse":
+                rctogether.send_message(
+                    self.genie.id,
+                    f"@**{adopter['person_name']}** Sorry, that's just a picture of a horse.",
+                )
                 return
             animal = self.get_by_name(animal_name)
             if not animal:
-                rctogether.send_message(self.genie.id, f"@**{adopter['person_name']}** Sorry, we don't have a {animal_name} at the moment, perhaps you'd like a {self.random_available_animal().name} instead?")
+                rctogether.send_message(
+                    self.genie.id,
+                    f"@**{adopter['person_name']}** Sorry, we don't have a {animal_name} at the moment, perhaps you'd like a {self.random_available_animal().name} instead?",
+                )
                 print("No such animal!")
                 return
             print(animal)
-            rctogether.send_message(animal.id, f"@**{adopter['person_name']}** {NOISES.get(animal.emoji, '💖')}")
-            rctogether.update_bot(animal.id, {'name': f"{adopter['person_name']}'s {animal.name}"})
+            rctogether.send_message(
+                animal.id,
+                f"@**{adopter['person_name']}** {NOISES.get(animal.emoji, '💖')}",
+            )
+            rctogether.update_bot(
+                animal.id, {"name": f"{adopter['person_name']}'s {animal.name}"}
+            )
             del self.available_animals[position_tuple(animal.bot_json["pos"])]
-           #  self.available_animals[position_tuple(animal.bot_json["pos"])] = self.spawn_animal(animal['pos'])
-            self.owned_animals[adopter['id']].append(animal)
+            #  self.available_animals[position_tuple(animal.bot_json["pos"])] = self.spawn_animal(animal['pos'])
+            self.owned_animals[adopter["id"]].append(animal)
 
             print("owned_animals: ", self.owned_animals)
 
             return
 
-        m = re.search(r'adopt (a|an|the|one)? ([A-Za-z-]+)', message['text'], re.IGNORECASE)
+        m = re.search(
+            r"adopt (a|an|the|one)? ([A-Za-z-]+)", message["text"], re.IGNORECASE
+        )
         if m:
-            rctogether.send_message(self.genie.id, f"@**{adopter['person_name']}** Our pets are only available to polite homes.")
+            rctogether.send_message(
+                self.genie.id,
+                f"@**{adopter['person_name']}** Our pets are only available to polite homes.",
+            )
             return
 
-        m = re.search("thank", message['text'], re.IGNORECASE)
+        m = re.search("thank", message["text"], re.IGNORECASE)
         if m:
             welcomes = ["You're welcome!", "No problem!", "❤️"]
-            rctogether.send_message(self.genie.id, f"@**{adopter['person_name']}** {random.choice(welcomes)}")
+            rctogether.send_message(
+                self.genie.id,
+                f"@**{adopter['person_name']}** {random.choice(welcomes)}",
+            )
             return
 
-        m = re.search("time to restock", message['text'], re.IGNORECASE)
+        m = re.search("time to restock", message["text"], re.IGNORECASE)
         if m:
             self.restock_inventory()
-            rctogether.send_message(self.genie.id, f"@**{adopter['person_name']}** New pets now in stock!")
+            rctogether.send_message(
+                self.genie.id, f"@**{adopter['person_name']}** New pets now in stock!"
+            )
             return
 
-        rctogether.send_message(self.genie.id, f"@**{adopter['person_name']}**, sorry I don't understand. Would you like to adopt a pet?")
+        rctogether.send_message(
+            self.genie.id,
+            f"@**{adopter['person_name']}**, sorry I don't understand. Would you like to adopt a pet?",
+        )
 
     def handle_entity(self, entity):
-        if entity['type'] == 'Avatar':
-            message = entity.get('message')
+        if entity["type"] == "Avatar":
+            message = entity.get("message")
 
-            if message and self.genie.id in message['mentioned_entity_ids']:
-                message_dt = datetime.datetime.strptime(message['sent_at'], "%Y-%m-%dT%H:%M:%SZ")
+            if message and self.genie.id in message["mentioned_entity_ids"]:
+                message_dt = datetime.datetime.strptime(
+                    message["sent_at"], "%Y-%m-%dT%H:%M:%SZ"
+                )
                 if message_dt <= self.processed_message_dt:
                     print("Skipping old message: ", message)
                 else:
                     self.handle_mention(entity, message)
                     self.processed_message_dt = message_dt
 
-        if entity['type'] == 'Avatar':
-            for animal in self.owned_animals.get(entity['id'], []):
+        if entity["type"] == "Avatar":
+            for animal in self.owned_animals.get(entity["id"], []):
                 print(entity)
-                position = offset_position(entity['pos'], random.choice(DELTAS))
+                position = offset_position(entity["pos"], random.choice(DELTAS))
                 print(f"Moving {animal} to {position}")
                 animal.update(position)
 
-        if entity['type'] in ('Avatar', 'Wall', 'Note', 'Desk', 'Bot', 'ZoomLink', 'AudioRoom', 'AudioBlock', 'RC::Calendar', 'Link'):
-            return
+
+DELTAS = [{"x": x, "y": y} for x in [-1, 0, 1] for y in [-1, 0, 1] if x != 0 or y != 0]
 
 
-DELTAS = [{'x': x, 'y': y} for x in [-1, 0, 1] for y in [-1, 0, 1] if x != 0 or y != 0]
 def offset_position(position, delta):
-    return {'x': position['x'] + delta['x'], 'y': position['y'] + delta['y']}
+    return {"x": position["x"] + delta["x"], "y": position["y"] + delta["y"]}
+
 
 # rctogether.clean_up_bots()
 
