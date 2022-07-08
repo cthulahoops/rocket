b9900b3984d70b0c1230575a204452b98edb8bb8
Adam Kelly <adam@cthulahoops.org>
2022-07-08 16:40:58 +0100

    Test handle adoption of an unavailable pet.

--- test_pets.py
+++ test_pets.py
@@ -7,8 +7,12 @@ from bot import Bot
 Post = namedtuple('Post', ('path', 'json'))
 
 class MockSession:
-    def __init__(self):
+    def __init__(self, get_data):
         self.posts = []
+        self.get_data = get_data
+
+    async def get(self, path):
+        return self.get_data[path]
 
     async def post(self, path, json):
         self.posts.append(Post(path, json))
@@ -16,14 +20,16 @@ class MockSession:
 
 @pytest.mark.asyncio
 async def test_thanks():
-    session = MockSession()
-
     genie_id = 178
-    genie = Bot({'id': genie_id})
-    available_animals = {}
-    owned_animals = {}
+    session = MockSession({
+        "bots": [{
+            'type': 'Bot',
+            'id': genie_id,
+            'emoji': "🧞",
+        }]
+    })
 
-    agency = pets.Agency(session, genie, available_animals, owned_animals)
+    agency = pets.Agency.create(session)
 
     await agency.handle_entity({
         'type': 'Avatar',
@@ -39,3 +45,39 @@ async def test_thanks():
     print(message['text'])
     assert message['bot_id'] == genie_id
     assert message['text'][len('@**Person Name** '):] in pets.THANKS_RESPONSES
+
+@pytest.mark.asyncio
+async def test_adopt_unavailable():
+    genie_id = 178
+
+    session = MockSession({
+        "bots": [{
+            'type': 'Bot',
+            'id': genie_id,
+            'emoji': "🧞",
+        },
+        {
+            'type': 'Bot',
+            'id': 39887,
+            'name': 'rocket',
+            'emoji': "🚀",
+            'pos': {'x': 1, 'y': 1}
+         }]
+    })
+
+    agency = await pets.Agency.create(session)
+
+    await agency.handle_entity({
+        'type': 'Avatar',
+        'id': 91,
+        'person_name': 'Person Name',
+        'message': {
+            'mentioned_entity_ids': [genie_id],
+            'sent_at': '2037-12-31T23:59:59Z', # More reasonable sent_at.
+            'text': 'adopt the dog, please!'
+        }})
+    print(session.posts)
+    message = session.posts[0].json
+    print(message['text'])
+    assert message['bot_id'] == genie_id
+    assert message['text'][len('@**Person Name** '):] == "Sorry, we don't have a dog at the moment, perhaps you'd like a rocket instead?"
